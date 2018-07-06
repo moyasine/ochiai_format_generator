d8cd4fa6d32934fc75145cdcb40d621e451402fd
Natsuo Kawai <kawai.natsuo@gmail.com>
2018-07-06 11:03:21 +0900

    Update sample.rb

--- sample.rb
+++ sample.rb
@@ -5,6 +5,6 @@ height = 900
 g = OchiaiFormatGenerator.new(width, height)
 g.set_title('タイトルTitleタイトルTitleタイトル')
 g.set_author('著者Author著者')
-g.set_image("sample_picture.png")
+# g.set_image("sample_picture.png")
 g.set_content(100, height*2/5+250, "ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ")
 g.save_to_png('sample_format')
